075c8d99c081256c598f57f49139fe65b6374fd4
vanchung1234 <vuchung682001@gmail.com>
2022-07-17 23:12:39 +0700

    done

--- client/src/component/layout/Slider.js
+++ client/src/component/layout/Slider.js
@@ -5,7 +5,7 @@ import { useState } from "react";
 
 const useAudio = (url) => {
     const [audio] = useState(new Audio(url));
-    const [playing, setPlaying] = useState(false);
+    const [playing, setPlaying] = useState(true);
 
     const toggle = () => setPlaying(!playing);
 
