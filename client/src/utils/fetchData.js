99194dd3d508c2e91364e61937c0e93064a5da92
vanchung1234 <vuchung682001@gmail.com>
2022-07-17 20:08:51 +0700

    done

--- client/src/utils/fetchData.js
+++ client/src/utils/fetchData.js
@@ -1,35 +1,35 @@
 import axios from "axios";
 
 export const getDataAPI = async (url, token) => {
-    const res = await axios.get(`http://localhost:5000/api/${url}`, {
+    const res = await axios.get(`https://mern-shop-chungg.herokuapp.com/api/${url}`, {
         headers: { Authorization: token },
     });
     return res;
 };
 
 export const postDataAPI = async (url, post, token) => {
-    const res = await axios.post(`http://localhost:5000/api/${url}`, post, {
+    const res = await axios.post(`https://mern-shop-chungg.herokuapp.com/api/${url}`, post, {
         headers: { Authorization: token },
     });
     return res;
 };
 
 export const putDataAPI = async (url, post, token) => {
-    const res = await axios.put(`http://localhost:5000/api/${url}`, post, {
+    const res = await axios.put(`https://mern-shop-chungg.herokuapp.com/api/${url}`, post, {
         headers: { Authorization: token },
     });
     return res;
 };
 
 export const patchDataAPI = async (url, post, token) => {
-    const res = await axios.patch(`http://localhost:5000/api/${url}`, post, {
+    const res = await axios.patch(`https://mern-shop-chungg.herokuapp.com/api/${url}`, post, {
         headers: { Authorization: token },
     });
     return res;
 };
 
 export const deleteDataAPI = async (url, token) => {
-    const res = await axios.delete(`http://localhost:5000/api/${url}`, {
+    const res = await axios.delete(`https://mern-shop-chungg.herokuapp.com/api/${url}`, {
         headers: { Authorization: token },
     });
     return res;
